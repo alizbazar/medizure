23dc2f68fefd14a49760cb0713012eebb6f1a3d3
Albert Nazander <alizbazar@alizweb.com>
2016-11-26 16:16:05 +0200

    Init integrating BLE with React Native

--- ios/CalendarManager.swift
+++ ios/CalendarManager.swift
@@ -7,16 +7,37 @@
 //
 
 import Foundation
-
+import CoreBluetooth
 // CalendarManager.swift
 
 @objc(CalendarManager)
-class CalendarManager: NSObject {
+class CalendarManager:
+  NSObject {
   
   @objc(addEvent:location:date:)
   func addEvent(_ name: String, location: String, date: NSNumber) -> Void {
     // Date is ready to use!
     print("This is a test!!!!", name, location, date)
+    BluetoothConnector()
   }
   
+}
+
+class BluetoothConnector:
+  NSObject,
+  CBCentralManagerDelegate,
+  CBPeripheralDelegate {
+  
+  var centralManager : CBCentralManager!
+  
+  override init() {
+    super.init()
+    print("Täähän toimii!")
+    centralManager = CBCentralManager(delegate :self, queue: nil)
+  }
+ 
+  func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    centralManager.scanForPeripherals(withServices: nil, options: nil)
+    print("Hello world!")
+  }
 }
